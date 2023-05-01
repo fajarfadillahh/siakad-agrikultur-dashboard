2216135d28f980bf5986a50a77d2629555b65770
fajarfadillahh <fajaragustian979@gmail.com>
2023-05-01 20:04:04 +0700

    create pages & configuration routing pages at the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,38 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+// import pages
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import New from "./pages/new/New";
+import Single from "./pages/single/Single";
+
+import Login from "./pages/login/Login";
 
 const App = () => {
   return (
-    <div className="flex h-screen w-full items-center justify-center">
-      <h1 className="text-lg font-bold text-purple-500">hello world</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/">
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+
+          {/* user page routes */}
+          <Route path="users">
+            <Route index element={<List />} />
+            <Route path=":userId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+
+          {/* product page routes */}
+          <Route path="products">
+            <Route index element={<List />} />
+            <Route path=":productId" element={<Single />} />
+            <Route path="new" element={<New />} />
+          </Route>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 };
 
