ed918c0b94e52f0f7ceba19c7dc2a0601516fc47
fajarfadillahh <fajaragustian979@gmail.com>
2023-05-02 00:18:28 +0700

    modified: Navbar components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -10,31 +10,31 @@ const Navbar = () => {
     <div className="flex h-[70px] items-center border-b border-gray-100">
       <div className="flex w-full items-center justify-between px-[20px]">
         <div className="relative flex items-center">
-          <MdOutlineSearch className="absolute left-3 text-[20px] text-purple-600" />
+          <MdOutlineSearch className="absolute left-3 text-[20px] text-gray-500" />
           <input
             type="text"
             placeholder="Search..."
-            className="w-[280px] rounded-md border border-gray-200 py-2 pl-9 pr-2 text-[14px] font-semibold text-gray-900 outline-none placeholder:text-[12px] placeholder:text-gray-500 focus:border-purple-600 focus:ring focus:ring-purple-100"
+            className="w-[280px] rounded-md border border-gray-300 py-2 pl-9 pr-2 text-[14px] font-semibold text-gray-900 outline-none placeholder:text-[12px] placeholder:text-gray-500 focus:border-purple-600 focus:ring focus:ring-purple-100"
           />
         </div>
 
-        <div className="items flex items-center gap-4">
-          <div className="item relative">
+        <div className="flex items-center gap-4">
+          <button className="relative cursor-pointer rounded-md p-[3px] hover:bg-gray-100 focus:ring-[3px] focus:ring-purple-500">
             <MdOutlineDarkMode className="text-[20px] text-gray-900" />
-          </div>
-          <div className="item relative">
+          </button>
+          <button className="relative cursor-pointer rounded-md p-[3px] hover:bg-gray-100 focus:ring-[3px] focus:ring-purple-500">
             <MdOutlineNotifications className="text-[20px] text-gray-900" />
-            <div className="absolute right-0 top-0 h-[10px] w-[10px] rounded-full bg-red-500" />
-          </div>
-          <div className="item relative">
-            <div className="h-[36px] w-[36px] cursor-pointer overflow-hidden rounded-full">
+            {/* <div className="absolute right-1 top-1 h-2 w-2 rounded-full bg-red-500" /> */}
+          </button>
+          <button className="relative cursor-pointer rounded-full focus:ring-[3px] focus:ring-purple-500">
+            <div className="h-[36px] w-[36px] overflow-hidden rounded-full">
               <img
                 src="/assets/images/avatar_5.jpg"
                 alt="profile picture"
                 className="h-full w-full object-cover object-center"
               />
             </div>
-          </div>
+          </button>
         </div>
       </div>
     </div>
