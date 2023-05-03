530b6658b3636df20f5db08f50d3e915fda34c6e
fajarfadillahh <fajaragustian979@gmail.com>
2023-05-03 13:32:11 +0700

    create: _mock folder for the Chart & Transactions data dummy

---
+++ src/_mock/ChartData.jsx
@@ -0,0 +1,10 @@
+const data = [
+  { name: "January", Total: 1284 },
+  { name: "February", Total: 2148 },
+  { name: "March", Total: 812 },
+  { name: "April", Total: 1629 },
+  { name: "May", Total: 917 },
+  { name: "June", Total: 1766 },
+];
+
+export default data;
--- src/components/chart/Chart.jsx
+++ src/components/chart/Chart.jsx
@@ -9,15 +9,8 @@ import {
   ResponsiveContainer,
 } from "recharts";
 
-// _mock chart data
-const data = [
-  { name: "January", Total: 1284 },
-  { name: "February", Total: 2148 },
-  { name: "March", Total: 812 },
-  { name: "April", Total: 1629 },
-  { name: "May", Total: 917 },
-  { name: "June", Total: 1766 },
-];
+// import chart data
+import data from "../../_mock/ChartData";
 
 const Chart = () => {
   return (
