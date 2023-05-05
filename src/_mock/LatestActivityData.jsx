ab948ff2aae0b4f3c8934b2d2cb7f8fe92877474
fajarfadillahh <fajaragustian979@gmail.com>
2023-05-05 22:45:47 +0700

    tracked file mock data

---
+++ src/_mock/LatestActivityData.jsx
@@ -0,0 +1,14 @@
+import { faker } from "@faker-js/faker";
+
+export const ActivityRows = [...Array(5)].map(() => ({
+  id: faker.random.numeric(7),
+  name: faker.name.fullName(),
+  email: faker.internet.email(),
+  numberId: faker.random.numeric(16),
+  avatarImg: faker.internet.avatar(),
+  date: faker.date.recent(),
+  address: faker.address.streetAddress(true),
+  status: faker.helpers.arrayElement(["Approved", "Pending"]),
+}));
+
+export default ActivityRows;
