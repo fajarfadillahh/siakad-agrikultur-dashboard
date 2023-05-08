6a6813ac36abcbb6dd5b903dac9f1454c856cabb
fajarfadillahh <fajaragustian979@gmail.com>
2023-05-08 23:06:46 +0700

    fixed: List page / Users page

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // import pages
 import Home from "./pages/home/Home";
+import Users from "./pages/list/List";
 
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route index element={<Home />} />
+        <Route path="/users" element={<Users />} />
       </Routes>
     </Router>
   );
--- src/pages/list/List.jsx
+++ src/pages/list/List.jsx
@@ -1,20 +1,33 @@
 import React from "react";
+import { MdAdd } from "react-icons/md";
 
 // import components
-import Sidebar from "../../components/sidebar/SidebarMobile";
-import Navbar from "../../components/navbar/Navbar";
+import Layout from "../../components/layout/Layout";
 import Datatable from "../../components/datatable/Datatable";
 
 const List = () => {
   return (
-    <div className="flex w-full">
-      <Sidebar />
+    <Layout>
+      <div className="grid gap-4 rounded-md border border-gray-100 p-5 shadow-3xl">
+        <div className="flex items-center justify-between">
+          <h1 className="font-medium capitalize text-gray-500">
+            List all users
+          </h1>
 
-      <div className="flex-[6]">
-        <Navbar />
-        <Datatable />
+          <button
+            className="inline-flex items-center justify-between gap-2 rounded-md bg-purple-600 px-4 py-2 text-[14px] font-medium text-white hover:bg-purple-700"
+            onClick={(e) => e.preventDefault()}
+          >
+            <MdAdd className="text-[20px]" />
+            Add User
+          </button>
+        </div>
+
+        <div className="overflow-x-auto">
+          <Datatable />
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 };
 
